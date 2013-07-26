1722ed82edb9144ec43b83c5881bdfea6d305287
James Marshall <jcmarsh@gwmail.gwu.edu>
2013-07-26 16:39:40 -0400

    New png generator for the door scenario. Unused var removed from uturn.py.

--- grid_draw/door.py
+++ grid_draw/door.py
@@ -3,12 +3,14 @@ import ImageDraw
 import sys
 
 file_name = sys.argv[1]
-line_width = int(sys.argv[2])
-print "File name: %s\t Alley width: %d" % (file_name, line_width)
+print "File name: %s" % (file_name)
 
 max_x = 500
 max_y = 500
 start_margin = 100
+line_width = 64
+robot_width = 16
+
 
 im = Image.new("1", (max_x, max_y))
 
@@ -24,8 +26,11 @@ draw.rectangle((0, max_y, max_x, max_y - line_width), fill=1)
 draw.rectangle((max_x, 0, max_x - line_width, max_y), fill=1)
 
 # Draw the promising dead end.
-draw.rectangle((start_margin, start_margin, start_margin + line_width, max_y - line_width), fill=1)
-draw.rectangle((start_margin, start_margin, max_x - 2 * start_margin, start_margin + line_width), fill=1)
+draw.rectangle((start_margin, max_y - line_width, start_margin + line_width, max_y - 3 * line_width), fill=1)
+draw.rectangle((start_margin + line_width, max_y - 3 * line_width, max_x, max_y - 2 * line_width), fill=1)
+
+# Slot for the door robot.
+draw.rectangle((start_margin+line_width, max_y-3*line_width, start_margin+line_width+robot_width, max_y-4*line_width), fill=1)
 
 # Create an obstacle in the upper left corner and the lower right corner
 # This is needed to keep player stage from contracting the map.
