89bffdee413b2f96bd1df46d611a985250310ac7
James <jcmarsh@gwmail.gwu.edu>
2014-03-07 11:19:00 -0500

    Makefile compiles with -g. test.c attempts to intercept a syscall... and fails.

--- ptrace/test.c
+++ ptrace/test.c
@@ -1,73 +1,91 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <sys/ptrace.h>
-
 #include <sys/reg.h>
+#include <sys/user.h>
+#include <sys/wait.h>
 
-const char *reg_names[27];
-
-void init_reg_names() {
-  reg_names[0] = "R15";
-  reg_names[1] = "R14";
-  reg_names[2] = "R13";
-  reg_names[3] = "R12";
-  reg_names[4] = "RBP";
-  reg_names[5] = "RBX";
-  reg_names[6] = "R11";
-  reg_names[7] = "R10";
-  reg_names[8] = "R9";
-  reg_names[9] = "R8";
-  reg_names[10] = "RAX";
-  reg_names[11] = "RCX";
-  reg_names[12] = "RDX";
-  reg_names[13] = "RSI";
-  reg_names[14] = "RDI";
-  reg_names[15] = "ORIG_RAX";
-  reg_names[16] = "RIP";
-  reg_names[17] = "CS";
-  reg_names[18] = "EFLAGS";
-  reg_names[19] = "RSP";
-  reg_names[20] = "SS";
-  reg_names[21] = "FS_BASE";
-  reg_names[22] = "GS_BASE";
-  reg_names[23] = "DS";
-  reg_names[24] = "ES";
-  reg_names[25] = "FS";
-  reg_names[26] = "GS";
-}
-
-void allregs(pid) {
-  long reg;
-  int index;
-
-  for (index = 0; index < 27; index++) {
-    reg = ptrace(PTRACE_PEEKUSER, pid, 8 * index, NULL);
-    printf("Reg: %s = %ld\n", reg_names[index], reg);
-  }
+void printREGS(struct user_regs_struct * regs) {
+  printf("R15: %lX\n", regs->r15);
+  printf("R14: %lX\n", regs->r14);
+  printf("R13: %lX\n", regs->r13);
+  printf("R12: %lX\n", regs->r12);
+  printf("RBP: %lX\n", regs->rbp);
+  printf("RBX: %lX\n", regs->rbx);
+  printf("R11: %lX\n", regs->r11);
+  printf("R10: %lX\n", regs->r10);
+  printf("R9:  %lX\n", regs->r9);
+  printf("R8:  %lX\n", regs->r8);
+  printf("RAX: %lX\n", regs->rax);
+  printf("RCX: %lX\n", regs->rcx);
+  printf("RDX: %lX\n", regs->rdx);
+  printf("RSI: %lX\n", regs->rsi);
+  printf("RDI: %lX\n", regs->rdi);
+  printf("ORIG_RAX: %lX\n", regs->orig_rax);
+  printf("RIP: %lX\n", regs->rip);
+  printf("CS:  %lX\n", regs->cs);
+  printf("EFLAGS: %lX\n", regs->eflags);
+  printf("RSP: %lX\n", regs->rsp);
+  printf("SS:  %lX\n", regs->ss);
+  printf("FS_BASE: %lX\n", regs->fs_base);
+  printf("GS_BASE: %lX\n", regs->gs_base);
+  printf("DS:  %lX\n", regs->ds);
+  printf("ES:  %lX\n", regs->es);
+  printf("FS:  %lX\n", regs->fs);
+  printf("GS:  %lX\n", regs->gs);
 }
 
 int main(int argc, char** argv) {
-  pid_t childPID = -1; pid_t currentPID = 0;
+  pid_t child_pid = -1;
+  int status = -1;
   long reg;
+  int ret_value = 0;
+  struct user_regs_struct child_regs;
+  struct user_regs_struct copy_regs;
 
-  childPID = fork();
+  child_pid = fork();
 
-
-  init_reg_names();
-
-  if (childPID >=0) { // Successful fork
-    if (childPID == 0) {
+  if (child_pid >=0) { // Successful fork
+    if (child_pid == 0) {
       printf("I am the child\n");
       ptrace(PTRACE_TRACEME, 0, NULL, NULL);
       execl("/bin/ls", "ls", NULL);
     } else {
       printf("I am the parent\n");
-      currentPID = wait(NULL);
-      //     reg = ptrace(PTRACE_PEEKUSER, childPID, 8 * ORIG_RAX, NULL);
-      allregs(currentPID);
-      //printf("Whoa son, %ld syscall\n", reg);
-      ptrace(PTRACE_CONT, childPID, NULL, NULL);
+
+      while(1) {
+	waitpid(child_pid, &status, 0);
+
+	if(WIFEXITED(status) || WIFSIGNALED(status)) {
+	  break;
+	}
+
+	if (ptrace(PTRACE_GETREGS, child_pid, NULL, &child_regs) < 0) {
+	  printf("GETREGS error.\n");
+	  return 0;
+	}
+
+	printREGS(&child_regs);
+
+	child_regs.r8 = 0xDEADBEEF;
+	child_regs.orig_rax = 0x1;
+
+	if (ptrace(PTRACE_SETREGS, child_pid, NULL, &child_regs) < 0) {
+	  printf("SETREGS error.\n");
+	  return 0;
+	}
+
+	if (ptrace(PTRACE_GETREGS, child_pid, NULL, &child_regs) < 0) {
+	  printf("GETREGS error.\n");
+	  return 0;
+	}
+
+	printREGS(&child_regs);
+
+	ptrace(PTRACE_CONT, child_pid, NULL, NULL);
+      }
     }
   }
   return 0;
