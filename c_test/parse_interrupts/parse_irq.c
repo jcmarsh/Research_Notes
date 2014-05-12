d9abe03c1d0889ca0a6e7422d1affa8861394cb5
James Marshall <jcmarsh@gwmail.gwu.edu>
2014-05-12 17:55:48 -0400

    Timing of timer ticks going well... but does not explain previous problems.

--- c_test/parse_interrupts/parse_irq.c
+++ c_test/parse_interrupts/parse_irq.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include "taslimited.h"
 
 #define rdtscll(value)				       \
   __asm__ ("rdtsc\n\t"				       \
@@ -16,47 +17,85 @@
 #define TARGET_ROW 15
 #define TARGET_COL 2
 
-int oldParse() {
+FILE* proc_interrupts;  
+
+long oldParse() {
   const char token[2] = " ";
-  FILE* proc_interrupts;  
   char buffer[256];
   int row_num = 0;
   int col_num = 0;
   
   char *cell_value;
 
-  proc_interrupts = fopen("/proc/interrupts", "r");
+  rewind(proc_interrupts);
 
   while(fgets(buffer, 256, proc_interrupts) > 0) {
     if (row_num == TARGET_ROW) {
-      printf("Line %d: %s\n", row_num, buffer);
       cell_value = strtok(buffer, token);
-      printf("Sigh: %s\n", cell_value);
       while (cell_value != NULL) {
 	cell_value = strtok(NULL, token);
 	if (col_num == TARGET_COL) {
-	  printf("cell_value: %s\n", cell_value);
+	  return atol(cell_value);
 	}
 	col_num++;
       }
     }
     row_num++;
   }
+  return 0;
 }
 
 int main(int argc, char ** argv) {
-  unsigned long start;
-  unsigned long end;
+  unsigned long curr_time;
+  unsigned long last_time;
+  unsigned long start_time;
+  unsigned long end_time;
   int index = 0;
-  int tests = 100;
+  int tests = 100000;
   long total_time;
+  long curr_int_count;
+  long last_int_count;
+  long int_estimate = 0;
+  
+
+  long time_guess = 5000;
+  bool results_tick[tests];
+  long results_time[tests];
+
+  cpu_speed_t cpu_speed;
+
+  InitTAS(DEFAULT_CPU, &cpu_speed, 0);
+  proc_interrupts = fopen("/proc/interrupts", "r");
+
+  last_int_count = oldParse();
+  rdtscll(start_time);
+  rdtscll(last_time);
+  
+  for (index = 0; index < tests; index++) {
+    rdtscll(curr_time);
+    
+    results_time[index] = curr_time - last_time;
+    if (results_time[index] > time_guess) {
+      results_tick[index] = true;
+      //      int_estimate++;
+    } else {
+      results_tick[index] = false;
+    }
+    
+    last_time = curr_time;
+  }
+
+  rdtscll(end_time);
+  curr_int_count = oldParse();
 
   for (index = 0; index < tests; index++) {
-    rdtscll(start);
-    oldParse();
-    rdtscll(end);
-    total_time += end - start;
+    if (results_tick[index]) {
+      printf("Timer tick: %ld\n", results_time[index]);
+      int_estimate++;
+    } else {
+      //      printf("No Timer tick: %ld\n", results_time[index]);
+    }
   }
-  
-  printf("Average: %lf\n", (1.0 * total_time) / tests);
+
+  printf("Estimate: %ld\tActual: %ld\t in %ld\n", int_estimate, curr_int_count - last_int_count, end_time - start_time);
 }
