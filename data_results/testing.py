0cf9aa50152f53a8ed4f55074732b07d64a24ce0
James Marshall <jcmarsh@gwmail.gwu.edu>
2014-05-22 14:58:59 -0400

    Minor change to python graphing script.

--- data_results/testing.py
+++ data_results/testing.py
@@ -1,7 +1,9 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-data_file = open("./TimingTicks/HundredThousandInterruptions_new.txt", "r")
+print "Opening: ", sys.argv[1]
+data_file = open(sys.argv[1], "r")
 
 print data_file.readline()
 
@@ -10,22 +12,21 @@ x = []
 for index in range(0, 100000):
     x.append(int(data_file.readline()))
 
-#mu, sigma = 100, 15
-#x = mu + sigma * np.random.randn(10000)
+print max(x)
+x.remove(max(x))
+print max(x)
+x.remove(max(x))
 
 plt.figure(figsize=(12, 5))
 
 #n, bins, patches = plt.hist(x, 150, facecolor='g')
 
-n, bins, patches = plt.hist(x, 150, [0, 100000], facecolor='g')
+#n, bins, patches = plt.hist(x, 150, [0, 100000], facecolor='g')
+n, bins, patches = plt.hist(x, 150, facecolor='g')
 
 plt.xlabel('Time in Cycles')
 plt.ylabel('Count')
 plt.grid(True)
-
-print max(x)
-x.remove(max(x))
-print max(x)
 
 plt.show()
 
