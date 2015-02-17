ad24404f3ca40ff2390135a5f5b01bc58c96ac48
James <jcmarsh@gwmail.gwu.edu>
2015-02-17 15:29:32 -0500

    Expanded signal timing example to include mlockall code.

--- signal_timing/main.c
+++ signal_timing/main.c
@@ -1,6 +1,10 @@
+#define _GNU_SOURCE
+
+#include <sched.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <signal.h>
+#include <sys/mman.h>
 
 #define ORPHAN_TO_ANCESTOR (SIGRTMIN + 1)
 #define ANCESTOR_TO_ORPHAN (SIGRTMIN + 2)
@@ -10,13 +14,56 @@
 	   "shl $(32), %%rdx\n\t"		       \
 	   "or %%rax, %%rdx" : "=d" (value) : : "rax")
 
-static void signalHandler(int signo, siginfo_t *si, void *unused) {
-	// get time
+int lockItUp(void) {
+	// Must be super user.
+	if (mlockall(MCL_CURRENT || MCL_FUTURE) == -1) { // Lock current pages and future ones.
+		perror("mlockall failed");
+		return -1;
+	}
+
+	return 0;
+}
+
+int cpu_bind( const pid_t pid, const int cpu ) {
+  cpu_set_t cpuset_mask;
+  // zero out the cpu set
+  CPU_ZERO( &cpuset_mask );
+  // bind the processor with the zeroed mask 
+  CPU_SET( cpu, &cpuset_mask );
+
+  // set the affinity of the process to the mask containing the processor
+  // this effectively binds the process to the processor
+  if( sched_setaffinity( pid, sizeof(cpuset_mask), &cpuset_mask ) == -1 ) {
+    return -1;
+  }
+
+  return 0;
+}
+
+void initProcess(void) {
+	if (cpu_bind(getpid(), 1) < 0) {
+		perror("Parent cpu bind failed");
+	}
+
+	unsigned long last_time;
+	rdtscll(last_time);
+	
+	if (lockItUp() < 0) {
+		perror("Failed to lockItUp");
+	}
+	
 	unsigned long curr_time;
 	rdtscll(curr_time);
-	unsigned long parent_time = (unsigned long)si->si_value.sival_ptr;
+	printf("(%ld)\n", curr_time - last_time);
+}
+
+static void signalHandler(int signo, siginfo_t *si, void *unused) {
+	// get time
+	//unsigned long curr_time;
+	//rdtscll(curr_time);
+	//unsigned long parent_time = (unsigned long)si->si_value.sival_ptr;
 	//printf("Signal time: %ld\tParent time: %ld\tTime elapsed: %ld\n", curr_time, parent_time, curr_time - parent_time);
-	printf("(%ld)\n", curr_time - parent_time);
+	//printf("(%ld)\n", curr_time - parent_time);
 }
 
 pid_t child_pid;
@@ -28,9 +75,11 @@ static void parentHandler(int signo, siginfo_t *si, void *unused) {
 	printf("New child pid set to: %d\n", child_pid);
 }
 
-int child_counter = 10000;
+int child_counter = 10;
 
 void fork_n_die(void) {
+	initProcess();
+
 	// child process, create orphan
 	child_pid = fork();
 	if (child_pid == -1) {
@@ -41,6 +90,8 @@ void fork_n_die(void) {
 		if (child_counter > 0) {
 			fork_n_die();
 		} else {
+			initProcess();
+
 			printf("Soon to be Orphan here, signal to %d\n", ancestor_pid);
 			// child process
 			// setup handler first so will not terminate when signal recieved.
@@ -68,6 +119,8 @@ void fork_n_die(void) {
 }
 
 int main(int argc, char** argv) {
+	initProcess();
+
 	ancestor_pid = getpid();
 	child_pid = fork();
 
@@ -91,11 +144,11 @@ int main(int argc, char** argv) {
 		while (1) {
 			usleep(100000);
 			// get time
-			unsigned long curr_time;
-			rdtscll(curr_time);
-			union sigval time_value;
-			time_value.sival_ptr = (void *)curr_time;
-			sigqueue(child_pid, ANCESTOR_TO_ORPHAN, time_value);
+			//unsigned long curr_time;
+			//rdtscll(curr_time);
+			//union sigval time_value;
+			//time_value.sival_ptr = (void *)curr_time;
+			//sigqueue(child_pid, ANCESTOR_TO_ORPHAN, time_value);
 		}
 	}
 }
--- signal_timing/simple_test.c
+++ signal_timing/simple_test.c
@@ -1,11 +1,50 @@
+#define _GNU_SOURCE
+
+#include <sched.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <signal.h>
+#include <sys/mman.h>
 
 #define rdtscll(value)				       \
   __asm__ ("rdtsc\n\t"				       \
 	   "shl $(32), %%rdx\n\t"		       \
 	   "or %%rax, %%rdx" : "=d" (value) : : "rax")
+
+int lockItUp(void) {
+	// Must be super user.
+	if (mlockall(MCL_CURRENT || MCL_FUTURE) == -1) { // Lock current pages and future ones.
+		perror("mlockall failed");
+		return -1;
+	}
+
+	return 0;
+}
+
+int cpu_bind( const pid_t pid, const int cpu ) {
+ 	cpu_set_t cpuset_mask;
+ 	// zero out the cpu set
+ 	CPU_ZERO( &cpuset_mask );
+ 	// bind the processor with the zeroed mask 
+ 	CPU_SET( cpu, &cpuset_mask );
+
+	// set the affinity of the process to the mask containing the processor
+	// this effectively binds the process to the processor
+	if( sched_setaffinity( pid, sizeof(cpuset_mask), &cpuset_mask ) == -1 ) {
+		return -1;
+	}
+
+	return 0;
+}
+
+void initProcess(void) {
+	if (cpu_bind(getpid(), 1) < 0) {
+		perror("Parent cpu bind failed");
+	}
+	if (lockItUp() < 0) {
+		perror("Failed to lockItUp");
+	}
+}
 
 static void signalHandler(int signo, siginfo_t *si, void *unused) {
 	// get time
@@ -17,11 +56,15 @@ static void signalHandler(int signo, siginfo_t *si, void *unused) {
 }
 
 int main(int argc, char** argv) {
+	initProcess();
+
 	pid_t child_pid = fork();
 	if (child_pid == -1) {
 		perror("First fork failed");
 		return -1;
 	} else if (child_pid == 0) {
+		initProcess();
+
 		// child process - setup handler. Recv signals and print times
 		struct sigaction sa;
 
