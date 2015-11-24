2844f9048130014ed86f02f293bcb98012bed5e5
James <jcmarsh@gwmail.gwu.edu>
2015-11-24 13:56:14 -0500

    Added test cycle count for arm systems.

---
+++ misc_test/c_test/cycle_count/p15.c
@@ -0,0 +1,39 @@
+// Found code: http://gperftools.googlecode.com/svn/trunk/src/base/cycleclock.h
+// Another reference: http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0290g/Bihcgfcf.html 
+
+#include <stdio.h>
+#include <stdint.h>
+
+// This should return the number of cycles since power-on.  Thread-safe.
+static inline int64_t Now() {
+	uint32_t pmccntr;
+	uint32_t pmuseren;
+	uint32_t pmcntenset;
+	// Read the user mode perf monitor counter access permissions.
+	asm volatile ("mrc p15, 0, %0, c9, c14, 0" : "=r" (pmuseren));
+	if (pmuseren & 1) {  // Allows reading perfmon counters for user mode code.
+		asm volatile ("mrc p15, 0, %0, c9, c12, 1" : "=r" (pmcntenset));
+		if (pmcntenset & 0x80000000ul) {  // Is it counting?
+			asm volatile ("mrc p15, 0, %0, c9, c13, 0" : "=r" (pmccntr));
+			// The counter is set up to count every 64th cycle
+			return (int64_t)((pmccntr) * 64);  // Should optimize to << 6
+		}
+    }
+}
+
+int main(int charc, char* argv[]) {
+	printf("Test program for cycle count on ARM using p15\n");
+	int64_t current, last;
+	int i, sum = 0;
+
+	last = Now();
+	for (i = 0; i < 10000; i++) {
+		// loop to take some time.
+		sum += i;
+	}
+	current = Now();
+
+	printf("Loop took: %lld cycles.\n", current - last);
+
+	return 0;
+}
