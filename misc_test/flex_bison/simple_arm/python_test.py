4f3d635ddc949b8a9349abdffc2e71267322ddde
James Marshall <jcmarsh@gwmail.gwu.edu>
2017-09-29 08:35:24 -0400

    Basic python calling to ctypes working.

--- misc_test/flex_bison/simple_arm/python_test.py
+++ misc_test/flex_bison/simple_arm/python_test.py
@@ -2,3 +2,11 @@ import ctypes
 
 print "You should get this working."
 
+ctypes.cdll.LoadLibrary("libarmparse.so")
+libarm = ctypes.CDLL("libarmparse.so")
+
+result = ctypes.c_char_p("This should be replaced by the desired registers.")
+x = libarm.parse_line(result, "0x00100738 0xe51b2008      STR r2, [r11, #-0x8]\n")
+
+print result.value
+
