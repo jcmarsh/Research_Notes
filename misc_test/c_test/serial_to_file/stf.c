434d77ed19d3ce95d82ef2ec2528e3edd99c35db
James Marshall <jcmarsh@gwmail.gwu.edu>
2018-01-26 20:37:43 -0500

    Finish C serial read program.

--- misc_test/c_test/serial_to_file/stf.c
+++ misc_test/c_test/serial_to_file/stf.c
@@ -1,5 +1,9 @@
 // stf.c
-// Read from a serial port to a file until the specified string is detected.
+// Read from a serial port until the specified string is detected.
+//
+// Buffer size is set to 1024. Works if stop string is split between reads.
+// (tested with buffer size of 1)
+//
 // James Marshall
 
 #include <errno.h>
@@ -7,8 +11,10 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
-
+#include <stdbool.h>
 #include <stdio.h>
+
+#define BUFF_SIZE 1024
 
 // Nabbing some of this code (especiall set_interfac_attribs) from wallyk https://stackoverflow.com/a/6947758/1601162
 
@@ -49,47 +55,57 @@ int set_interface_attribs(int fd, int speed, int parity) {
   return 0;
 }
 
-#define BUFF_SIZE 1024
-
 int main(int argc, char ** argv) {
-  // TODO: args should be the port, output file name, and search string
-  char *portname = "/dev/ttyUSB1";
-  char *search_string = "safeword";
-  char *output_fn = "stf_output.txt";
-  
-  int output_fd = open(output_fn, O_CREAT | O_WRONLY);
-  if (output_fd < 0) {
-    perror("Yeah?");
-    printf("Failed to open output text file\n");
+
+  if (argc < 3) {
+    fprintf(stderr, "Usage: serial_to_file <serial_port> <stop_string>\n");
     return -1;
   }
 
+  char *portname = argv[1];
+  char *search_string = argv[2];
+
   int serial_fd = open(portname, O_RDONLY | O_NOCTTY | O_SYNC);
   if (serial_fd < 0) {
-    printf("Failed to open serial port\n");
+    fprintf(stderr, "Failed to open serial port\n");
     return -1;
   }
 
   set_interface_attribs(serial_fd, B460800, 0);  // set speed to 115,200 bps, 8n1 (no parity)
   //  set_blocking (fd, 0);                // set no blocking
 
-  //write (fd, "hello!\n", 7);           // send 7 character greeting
+  int read_cnt = 0;
+  int write_cnt = 0;
+  char buff[BUFF_SIZE];
+  int index = 0;
+  int found_index = 0;
+  bool done = false;
 
-  //usleep ((7 + 25) * 100);             // sleep enough to transmit the 7 plus
-                                       // receive 25:  approx 100 uS per char transmit
+  while (!done) {
+    read_cnt = read(serial_fd, buff, sizeof(buff));
+    if (read_cnt) {
 
-  int n = 0;
-  char buf[BUFF_SIZE];
+      write_cnt = write(STDOUT_FILENO, buff, read_cnt);
+      if (write_cnt != read_cnt) {
+        fprintf(stderr, "\n\nRead / Write Error\n\n");
+        return -1;
+      }
 
-  while (1) {
-    n = read(serial_fd, buf, sizeof(buf));  // read up to 100 characters if ready
-    if (n) {
-      printf("n: %d\n", n);
-      printf("%s\n", buf);
+      // See if the stop string is in buffer... keeping in mind it might be between buffers.
+      // Either I'm too clever or I'm too tired: I think this code elegantly handles the corner case.
+      for (index = 0; index < BUFF_SIZE; index++) {
+        if (buff[index] == search_string[found_index]) {
+          printf("\nFound a %c\n", search_string[found_index]);
+          found_index++;
+          if (found_index == strlen(search_string)) {
+            done = true;
+          }
+        } else {
+          found_index = 0;
+        }
+      }
     }
   }
-
-  printf("Output: %s\n", buf);
 
   return 0;
 }
