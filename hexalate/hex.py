22ddc2bf7061d9a40c76fb839456ba1bfd086a18
James Marshall <jcmarsh@gwmail.gwu.edu>
2018-03-02 19:30:10 -0500

    Hexalate reads from a config file.

--- hexalate/hex.py
+++ hexalate/hex.py
@@ -1,20 +1,26 @@
+import ConfigParser
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
 
-if len(sys.argv) < 3:
-	print "Usage: python hex.py <image_file.jpg> <width> <height>"
+if len(sys.argv) < 1:
+	print "Usage: python hex.py config.ini"
 	exit()
 
-print "Opening: ", sys.argv[1], "\tSize: ", sys.argv[2], ", ", sys.argv[3]
+print "Opening: ", sys.argv[1]
 
-width = int(sys.argv[2])
-height = int(sys.argv[3])
+config_file = sys.argv[1]
+my_config = ConfigParser.ConfigParser()
+my_config.readfp(open(config_file)) # TODO: error check
+
+file_name = my_config.get("image", "file_name")
+width = int(my_config.get("image", "width"))
+height = int(my_config.get("image", "height"))
 
 # convert file from jpg to some bit format.
-image = Image.open(sys.argv[1])
+image = Image.open(file_name)
 
 #image.show()
 
@@ -28,31 +34,45 @@ n = 0
 x = []
 y = []
 
+channel = int(my_config.get("processing", "channel"))
+levels = int(my_config.get("processing", "levels"))
+
+for l in range(levels - 1):
+        print "threshold:", l, " is", ((256 * 3) / levels) * (l + 1)
+
 for h in range(0, height):
         for w in range(0, width):
                 pixel = datas[(width * h) + w]
-                #print w, h, pixel
-                if (pixel[0] + pixel[1] + pixel[2]) < 500:
-                        x.append(w)
-                        y.append(height - h)
-                if (pixel[0] + pixel[1] + pixel[2]) < 350:
-                        x.append(w)
-                        y.append(height - h)
-                if (pixel[0] + pixel[1] + pixel[2]) < 200:
-                #if (pixel[0]) < 128:
-                        #print(w, h, pixel[1])
-                        x.append(w)
-                        #if (datas[(h * height) + w][1]) > 128:
-                        y.append(height - h)
-                #else:
-                        #print("Didn't make it", w, h, pixel[1])
+                if channel==4:
+                        for l in range(levels - 1):
+                                if (pixel[0] + pixel[1] + pixel[2]) < ((256 * 3) / levels) * (l + 1):
+                                        x.append(w)
+                                        y.append(height - h)
+                else:
+                        for l in range(levels - 1):
+                                if (pixel[channel] < (256 / levels) * (l + 1)):
+                                        x.append(w)
+                                        y.append(height - h)
 
-#plt.figure(figsize=(width, height))
+
+# Try to get rid of axes
+fig = plt.figure(frameon=False)
+#fig.set_size_pixels(width, height)
+ax = plt.Axes(fig, [0., 0., 1., 1.])
+ax.set_axis_off()
+fig.add_axes(ax)
+
 #plt.figure(figsize=((width / 300), (height / 300)))
 
-plt.hexbin(x, y, gridsize=80, cmap='gist_heat_r') #'gray_r')
-plt.axis([0, width, 0, height])
+out_file = my_config.get("output", "out_file")
+grid_size = int(my_config.get("output", "grid_size"))
+heat_map = my_config.get("output", "heat_map")
 
-plt.savefig("out.png", bbox_inches='tight') #, dpi=100)
+plt.hexbin(x, y, gridsize=grid_size, cmap=heat_map) #'gray_r')
+#plt.axis([0, width, 0, height])
+
+fig.savefig(out_file, dpi=300)
+#fig.savefig(out_file, bbox_inches='tight', dpi=300)
+#plt.savefig(out_file, bbox_inches='tight', dpi=300)
 
 plt.show()
