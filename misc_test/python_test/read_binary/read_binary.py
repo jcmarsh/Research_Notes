236f4c725648ba573511515bc315888d23fd13d3
James Marshall <jcmarsh@gwmail.gwu.edu>
2018-11-15 18:56:27 -0500

    Python reading binary file example.

---
+++ misc_test/python_test/read_binary/read_binary.py
@@ -0,0 +1,29 @@
+import sys
+
+# Read some integer values from an .ini file
+
+if len(sys.argv) < 2:
+	print("Usaged: python read_binary.py <file.pgm>")
+	exit()
+
+print("Opening: ", sys.argv[1])
+
+bin_file = open(sys.argv[1], "rb")
+
+while True:
+	char = bin_file.read(1)
+	#if char == b'':
+	#	break
+
+	try:
+		#con_char = str(c)
+		con_char = char.decode('utf-8')
+		print("Word: ", con_char)
+		if con_char == '':
+			break
+
+	except UnicodeDecodeError: # UnicodeEncodeError:
+		con_char = str(char)
+		print("Not Word: ", con_char)
+
+print("Ya done")
