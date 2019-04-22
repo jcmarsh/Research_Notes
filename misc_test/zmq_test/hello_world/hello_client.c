66eb6deb25fc207ea037d4f91752e408ce10c1c6
Marshall, James 459937851 <james.marshall-1@nasa.gov>
2019-04-22 15:30:50 -0400

    Timing for the client / server example.

--- misc_test/zmq_test/hello_world/hello_client.c
+++ misc_test/zmq_test/hello_world/hello_client.c
@@ -2,8 +2,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
+#define RUNS 5000
 int main(void) {
+  struct timespec start;
+  struct timespec end;
+  long elapsed_seconds = 0;
+  long elapsed_nseconds = 0;
+
   // Socked to talk to clients
   printf("Connecting to the server\n");
   void *context = zmq_ctx_new();
@@ -11,12 +18,19 @@ int main(void) {
   zmq_connect(requester, "tcp://localhost:5555");
 
   int request_nbr;
-  for (request_nbr = 0; request_nbr < 10; request_nbr++) {
+  for (request_nbr = 0; request_nbr < RUNS; request_nbr++) {
     char buffer[10];
-    printf("Sending Hello %d...\n", request_nbr);
+    // printf("Sending Hello %d...\n", request_nbr);
+    clock_gettime(CLOCK_REALTIME, &start);
     zmq_send(requester, "Hello", 5, 0);
     zmq_recv(requester, buffer, 10, 0);
-    printf("Received World %d\n", request_nbr);
+    clock_gettime(CLOCK_REALTIME, &end);
+    // printf("Received World %d\n", request_nbr);
+
+    elapsed_seconds = end.tv_sec - start.tv_sec;
+    elapsed_nseconds = end.tv_nsec - start.tv_nsec;
+    // printf("Time for function all: %lds and %ldns\n", elapsed_seconds, elapsed_nseconds);
+    printf("%ld\n", elapsed_nseconds);
   }
   zmq_close(requester);
   zmq_ctx_destroy(context);
